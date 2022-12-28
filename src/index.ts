9939bf779b75e4aa4b3959c367f4b8224010aab8
Thierry Seiler <>
2022-12-28 17:28:12 +0100

    worked on toggle

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-export * from "./components/Button/button";
-export * from "./components/Typo/typo";
+export * from './components/Button/button';
+export * from './components/Typo/typo';
+export * from './components/Toggle/toggle';
