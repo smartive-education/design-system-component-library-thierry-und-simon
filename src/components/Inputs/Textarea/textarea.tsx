4c4dfd803a4df298de62337eba73bba1144a43ce
eoss-sh <sh@eoss.ch>
2023-01-12 22:23:01 +0100

    styling of textarea

--- src/components/Inputs/Textarea/textarea.tsx
+++ src/components/Inputs/Textarea/textarea.tsx
@@ -2,8 +2,23 @@ import React, { FC } from "react";
 
 export type TextareaProps = {
   children: React.ReactNode;
+  placeholder: string;
+  cols: number;
 };
 
-export const Textarea: FC<TextareaProps> = ({ children }) => {
-  return <textarea />;
+export const Textarea: FC<TextareaProps> = ({
+  children,
+  placeholder,
+  cols = 30,
+}) => {
+  return (
+    <textarea
+      className="p-4 text-[20px] text-slate-900 placeholder:text-slate-500 bg-slate-100 rounded-lg resize-none border border-slate-200 hover:border-slate-300 hover:border-2 focus:border-2 focus:outline-none focus:border-violet-600"
+      rows={5}
+      cols={cols}
+      placeholder={placeholder}
+    >
+      {children}
+    </textarea>
+  );
 };
