4638b7ed788d73ea7b1384abae5fac68cabe866f
eoss-sh <sh@eoss.ch>
2023-01-12 23:00:46 +0100

    change input type and component elements

--- src/components/Forms/FileUpload/fileUpload.tsx
+++ src/components/Forms/FileUpload/fileUpload.tsx
@@ -15,14 +15,10 @@ export const FileUpload: FC<FileUploadProps> = ({
 }) => {
   return (
     <div className="">
-      <label className="block text-slate-700 font-bold " htmlFor={id}>
-        {label}
-      </label>
       <input
         className="bg-slate-50 p-2 text-[20px] text-slate-700 font placeholder:text-slate-500 rounded border border-slate-200 hover:border-slate-300 hover:border-2 focus:border-2 focus:outline-none focus:border-violet-600"
-        id={id}
-        type={type}
-        placeholder={placeholder}
+        type="file"
+        placeholder="...oder Datei auswählen"
       />
     </div>
   );
